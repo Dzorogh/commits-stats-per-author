0330ec9d33001d450bf11129ca64c8db64f88281
Sergey Indenbom <dzorogh@gmail.com>
2023-07-23 16:07:49 +0300

    chore: create secrets for github actions

--- .github/secrets/decrypt_secret.sh
+++ .github/secrets/decrypt_secret.sh
@@ -5,7 +5,7 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
---output $HOME/.env .env.gpg
+--output $HOME/.env ./.env.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
---output $HOME/appconfig.json appconfig.json.gpg
+--output $HOME/appconfig.json ./appconfig.json.gpg
