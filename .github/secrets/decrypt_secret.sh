9a60611fcb0c41b317206608a441c76f82f9ddc0
Sergey Indenbom <dzorogh@gmail.com>
2023-07-23 16:16:14 +0300

    chore: remove github actions

--- .github/secrets/decrypt_secret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
---output $GITHUB_WORKSPACE/.env $GITHUB_WORKSPACE/.github/secrets/.env.gpg
-
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
---output $GITHUB_WORKSPACE/appconfig.json $GITHUB_WORKSPACE/.github/secrets/appconfig.json.gpg
